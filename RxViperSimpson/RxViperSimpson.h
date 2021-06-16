a6c6fb9b50f2bb5d49aaddbe9873a21c672d8ac3
Faruuq <faruuq.q@icloud.com>
2021-06-16 14:30:46 +0700

    Initial Commit

---
+++ RxViperSimpson/RxViperSimpson.h
@@ -0,0 +1,18 @@
+//
+//  RxViperSimpson.h
+//  RxViperSimpson
+//
+//  Created by Muhammad Faruuq Qayyum on 16/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxViperSimpson.
+FOUNDATION_EXPORT double RxViperSimpsonVersionNumber;
+
+//! Project version string for RxViperSimpson.
+FOUNDATION_EXPORT const unsigned char RxViperSimpsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxViperSimpson/PublicHeader.h>
+
+
